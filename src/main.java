10d308deb4d066e7a412a6652673bc716493e259
TheHongster <aaronhong95@gmail.com>
2021-06-13 16:42:40 -0400

    initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @author Aaron Hong and Lynden Hill
+ * Group Project for ITCS-3166 at the University of North Carolina, Charlotte
+ */
+public class main {
+	public static void main(String[] args) {
+		
+    }
+}
