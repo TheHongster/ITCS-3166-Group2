398a7d37abfa588a8cfb9366c418f5f5313fec60
TheHongster <aaronhong95@gmail.com>
2021-06-19 18:32:43 -0400

    More GUI elements

--- src/GUI.java
+++ src/GUI.java
@@ -3,33 +3,51 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public abstract class GUI implements ActionListener {
+public class GUI extends JFrame {
 	public static void main(String[] args) {
+		//Variables
+		//Declarations
 		Main m = new Main();
+		JFrame f1 = new JFrame("IP Router");   
+	    	f1.setSize(400,400);  
+	    	f1.setLayout(null);  
+	    	f1.setVisible(true);  
+		JFrame f2 = new JFrame("Edit Routing Table Info");
+			f2.setSize(200,200);  
+			f2.setLayout(null);  
+			f2.setVisible(false); 
+	    TextField tfIP = new TextField();
+	    	tfIP.setBounds(105,105,150,20);
+	    TextField tfSubnetMask = new TextField();
+	    	tfSubnetMask.setBounds(105,105,150,20);
+	    	
+	    Button bSendData = new Button("Send Data");    
+	    	bSendData.setBounds(105,155,60,30);  
+	    	bSendData.addActionListener(new ActionListener() {
+	    		public void actionPerformed(ActionEvent e) {
+	    			m.IPAddress = tfIP.getText();
+	    		}
+	    	});
+	    	
+	    Button bEditData = new Button("Edit Data");
+	    	bEditData.setBounds(195,155,60,30);
+	    	bEditData.addActionListener(new ActionListener() {
+	    		public void actionPerformed(ActionEvent e) {
+	    			f2.setVisible(true); 
+	    		}
+	    	});
+	    //Frame formatting
+		f1.add(bSendData);f1.add(bEditData);f1.add(tfIP); 
+
 		//Logic
-		m.IPAddressBinary = m.convertToBinaryOctets(m.IPAddress);
+		/*m.IPAddressBinary = m.convertToBinaryOctets(m.IPAddress);
 		m.subnetMaskBinary = m.convertToBinaryOctets(m.subnetMask);
 			for(int i = 0; i < 4; i++) {//Assigns IPAddressBinary's values to networkAddressBinary values
 				m.networkAddressBinary[i] = m.IPAddressBinary[i];
 			}
 		m.calcBinaryNetworkAddress(m.networkAddressBinary, m.subnetMaskBinary);
 		m.networkAddress = m.convertToDecimalAddress(m.networkAddressBinary);
-
-		JFrame f = new JFrame("ActionListener Example");  
-	    TextField tf = new TextField();  
-	    tf.setBounds(105,105,150,20);  
-	    Button b=new Button("Click Here");  
-	    b.setBounds(50,100,60,30);  
-	    b.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	    		tf.setText("Welcome to Javatpoint."); 
-	    	}
-	    });
-	    f.add(b);f.add(tf);  
-	    f.setTitle("IP Router");
-	    f.setSize(400,400);  
-	    f.setLayout(null);  
-	    f.setVisible(true);   
+		*/
         
 	}
 }
